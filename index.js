bccf1345d4ffe0c140fe0c93c8a920436ce8c1b9
Christina Williams <christinaxtwilliams@gmail.com>
2020-10-06 14:31:26 -0400

    notes

--- index.js
+++ index.js
@@ -15,3 +15,11 @@ function largestSubarraySum(array) {
 
   return max;
 }
+
+
+// uses the fact that any time the running total goes below zero,
+  // we should reset the sum to 0, since the values 'behind' the index
+  // are 'hidden' by negative numbers
+
+  // handle case where value is a larger negative than the running total
+      // i.e. restart the sum at 0, indicating a restart of the subarray
